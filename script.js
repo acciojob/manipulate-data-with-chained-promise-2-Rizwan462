dc19fd8528f0769e4c2147b130fb489812765959
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-09-01 22:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function munipulateArray(){
+	return new Promise((resolve) => {
+		setTimeout(()=> {
+			const array = [1,2,3,4];
+			resolve(array);
+		}, 3000);
+	})
+	.then ((array) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const evenNum = array.filter(num => num%2===0);
+				document.getElementById('output').textContent = `Filtered array`
+				resolve(evenNum);
+			}, 1000);
+		})
+	})
+	.then((evenNum) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const multipliedNum = evenNum.map(num=>num*2);
+				document.getElementById('output').textContent = `Multiplied`
+				resolve(multipliedNum);
+			}, 2000)
+		})
+	})
+}
+munipulateArray();
+
+
+
+
